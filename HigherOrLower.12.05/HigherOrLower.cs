c95d37488dccb352aa1c3fa47a0a3aa0c23c37e2
emaGET <emanuelepini@getacademy.no>
2026-06-15 10:28:41 +0200

    my card in hand works

--- HigherOrLower.12.05/HigherOrLower.cs
+++ HigherOrLower.12.05/HigherOrLower.cs
@@ -1,8 +1,16 @@
 ﻿class HigherOrLower
 {
+    private bool isRunning = true ;
+    private int ccore = 0 ;
+    private int cardInHand = 0 ;
+    private int nextCard = 0 ;
+    
+    private Random rng = new Random();
+    
     public void Run()
     {
-        Console.WriteLine("Hi! Time to bet whether your number is higher - or lower!");
-        
+        Console.WriteLine("Hi! Time to bet whether your next card is higher or lower than the next card!");
+        cardInHand = rng.Next(1, 14);
+        Console.WriteLine("Your card is: " + cardInHand);
     }
 }
