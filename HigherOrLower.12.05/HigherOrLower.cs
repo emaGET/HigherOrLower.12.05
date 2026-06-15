b0b5ad09d4d5068a0558a761868e947627a88903
emaGET <emanuelepini@getacademy.no>
2026-06-15 10:43:52 +0200

    input to quit, done.

--- HigherOrLower.12.05/HigherOrLower.cs
+++ HigherOrLower.12.05/HigherOrLower.cs
@@ -1,16 +1,31 @@
 ﻿class HigherOrLower
 {
     private bool isRunning = true ;
-    private int ccore = 0 ;
+    private int score = 0 ;
     private int cardInHand = 0 ;
     private int nextCard = 0 ;
     
     private Random rng = new Random();
-    
+        
     public void Run()
     {
-        Console.WriteLine("Hi! Time to bet whether your next card is higher or lower than the next card!");
-        cardInHand = rng.Next(1, 14);
-        Console.WriteLine("Your card is: " + cardInHand);
+        while (isRunning)
+        {
+            
+            Console.WriteLine("Hi! Time to bet whether your next card is higher or lower than the next card!");
+            cardInHand = rng.Next(1, 14);
+            Console.WriteLine("Your card is: " + cardInHand);
+            Console.WriteLine("Press Q to quit, or anything else to continue.");
+            string input = Console.ReadLine();
+            
+            if (input.ToLower() == "Q")
+            {
+                isRunning = false ;
+            }
+        }
+        
+        Console.WriteLine("Thank you for gambling!");
+        
     }
+    
 }
